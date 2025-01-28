bc7b182c86e007213db95eb4420069641d936df5
Sara Panfini <sara.panfini@studio.unibo.it>
2025-01-28 11:15:54 +0100

    ciao

--- template/base.php
+++ template/base.php
@@ -7,8 +7,17 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" 
     rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css"/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
 </head>
 <body>
+    <header>
+        <i class="bi bi-list ps-3" style="color: brown; font-size: 50px;"></i>
+        <img src="img/img.png"/>
+        <i class="bi bi-bag pe-3" style="color: brown; font-size: 40px;"></i>
+    </header>
+    <footer>
+        <p>Informazioni su Mondo Morbidoso</p>
+    </footer>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" 
 integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
