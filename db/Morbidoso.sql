357bae3cfc0aaff1217d723c2e6e5a3fb01fe650
Sara Panfini <sara.panfini@studio.unibo.it>
2025-01-29 12:05:31 +0100

    uffaaaa

--- db/Morbidoso.sql
+++ db/Morbidoso.sql
@@ -3,143 +3,185 @@
 -- *--------------------------------------------
 -- * DB-MAIN version: 11.0.2              
 -- * Generator date: Sep 14 2021              
--- * Generation date: Wed Jan 22 12:06:14 2025 
+-- * Generation date: Tue Jan 28 16:00:10 2025 
+-- * LUN file: C:\Users\kolia\OneDrive - Alma Mater Studiorum Università di Bologna\Desktop\Tecnologie web\PROG\Morbidoso.lun 
+-- * Schema: Schema E/R/SQL 
 -- ********************************************* 
+
 
 -- Database Section
 -- ________________ 
 
-CREATE DATABASE Morbidoso;
-USE Morbidoso;
+create database Morbidoso;
+use Morbidoso;
+
+
+-- DBSpace Section
+-- _______________
 
 -- Tables Section
 -- _____________ 
 
-CREATE TABLE UTENTE (
-     E_mail CHAR(255) NOT NULL,
-     Nome CHAR(50) NOT NULL,
-     Cognome CHAR(50) NOT NULL,
-     Nome_utente CHAR(50) NOT NULL,
-     Numero_telefono CHAR(15) NOT NULL,
-     Data_di_nascita DATE,
-     Password CHAR(100) NOT NULL,
-     Punti INT NOT NULL,
-     PRIMARY KEY (E_mail)
-);
+create table UTENTE (
+     E_mail char(1) not null,
+     Nome char(1) not null,
+     Cognome char(1) not null,
+     Numero_telefono char(1) not null,
+     Data_di_nascita char(1),
+     Password char(1) not null,
+     Punti char(1) not null,
+     Admin char not null,
+     primary key (E_mail));
+     
+create table NOTIFICA (
+     Id_notifica char(1) not null,
+     Tipo_notifica char(1) not null,
+     Testo char(1) not null,
+     stato char(1) not null,
+     Giorno char(1) not null,
+     E_mail char(1) not null,
+     primary key (Id_notifica),
+     foreign key (E_mail) references UTENTE(E_mail));
 
-CREATE TABLE METODO_DI_PAGAMENTO (
-     Id_pagamento CHAR(50) NOT NULL,
-     Descrizione CHAR(255) NOT NULL,
-     PRIMARY KEY (Id_pagamento)
-);
+create table CATEGORIA (
+     Nome_categoria char(1) not null,
+     primary key (Nome_categoria));
 
-CREATE TABLE METODO_DI_SPEDIZIONE (
-     Id_spedizione CHAR(50) NOT NULL,
-     Nome CHAR(255) NOT NULL,
-     Descrizione CHAR(255) NOT NULL,
-     Costo DECIMAL(10,2),
-     PRIMARY KEY (Id_spedizione)
-);
+create table METODO_DI_PAGAMENTO (
+     Id_pagamento char(1) not null,
+     Descrizione char(1) not null,
+     primary key (Id_pagamento));
 
-CREATE TABLE INDIRIZZO_DI_FATTURAZIONE (
-     Id_fattura CHAR(50) NOT NULL,
-     Data_emissione DATE NOT NULL,
-     Importo_totale DECIMAL(10,2) NOT NULL,
-     Via CHAR(255) NOT NULL,
-     Civico CHAR(10) NOT NULL,
-     Cap CHAR(10) NOT NULL,
-     Citta CHAR(50) NOT NULL,
-     PRIMARY KEY (Id_fattura)
-);
+create table METODO_DI_SPEDIZIONE (
+     Id_spedizione char(1) not null,
+     Nome char(1) not null,
+     Descrizione char(1) not null,
+     Costo char(1),
+     primary key (Id_spedizione));
 
-CREATE TABLE ORDINE (
-     Id_ordine CHAR(50) NOT NULL,
-     Data_effettuazione DATE NOT NULL,
-     Prezzo_finale DECIMAL(10,2) NOT NULL,
-     Stato CHAR(20) NOT NULL,
-     Id_spedizione CHAR(50) NOT NULL,
-     Id_pagamento CHAR(50) NOT NULL,
-     Id_fattura CHAR(50) NOT NULL,
-     E_mail CHAR(255) NOT NULL,
-     PRIMARY KEY (Id_ordine),
-     FOREIGN KEY (Id_spedizione) REFERENCES METODO_DI_SPEDIZIONE(Id_spedizione),
-     FOREIGN KEY (Id_pagamento) REFERENCES METODO_DI_PAGAMENTO(Id_pagamento),
-     FOREIGN KEY (Id_fattura) REFERENCES INDIRIZZO_DI_FATTURAZIONE(Id_fattura),
-     FOREIGN KEY (E_mail) REFERENCES UTENTE(E_mail)
-);
+create table ORDINE (
+     Id_ordine char(1) not null,
+     Data_effettuazione char(1) not null,
+     Prezzo_finale char(1) not null,
+     Stato char(1) not null,
+     Id_spedizione char(1) not null,
+     Id_pagamento char(1) not null,
+     E_mail char(1) not null,
+     primary key (Id_ordine),
+     foreign key (Id_spedizione) references METODO_DI_SPEDIZIONE (Id_spedizione),
+     foreign key (E_mail) references UTENTE (E_mail),
+     foreign key (Id_pagamento) references METODO_DI_PAGAMENTO (Id_pagamento));
+    
+create table PRODOTTO (
+     Id_prodotto char(1) not null,
+     Nome char(1) not null,
+     Immagine char(1) not null,
+     Grandezza char(1) not null,
+     Scorta char(1) not null,
+     Prezzo char(1) not null,
+     Prezzo_punti char(1) not null,
+     Nome_categoria char(1) not null,
+     primary key (Id_prodotto),
+     foreign key (Nome_categoria) references CATEGORIA (Nome_categoria));
 
-CREATE TABLE PRODOTTO (
-     Id_prodotto CHAR(50) NOT NULL,
-     Nome CHAR(255) NOT NULL,
-     Immagine CHAR(255) NOT NULL,
-     Grandezza CHAR(20) NOT NULL,
-     Scorta INT NOT NULL,
-     Prezzo DECIMAL(10,2) NOT NULL,
-     Categoria CHAR(50) NOT NULL,
-     PRIMARY KEY (Id_prodotto)
-);
+create table carrello (
+     Id_prodotto char(1) not null,
+     E_mail char(1) not null,
+     Quantita char(1) not null,
+     primary key (E_mail, Id_prodotto),
+     foreign key (E_mail) references UTENTE (E_mail),
+     foreign key (Id_prodotto) references PRODOTTO (Id_prodotto));
 
-CREATE TABLE PRODOTTO_ORDINATO (
-     Id_prodotto_ordinato CHAR(50) NOT NULL,
-     Id_prodotto CHAR(50) NOT NULL,
-     PRIMARY KEY (Id_prodotto_ordinato),
-     FOREIGN KEY (Id_prodotto) REFERENCES PRODOTTO(Id_prodotto)
-);
+create table preferito (
+     Id_prodotto char(1) not null,
+     E_mail char(1) not null,
+     primary key (Id_prodotto, E_mail),
+     foreign key (E_mail) references UTENTE (E_mail),
+     foreign key (Id_prodotto) references PRODOTTO (Id_prodotto));
 
-CREATE TABLE DETTAGLIO_ACQUISTO (
-     Id_ordine CHAR(50) NOT NULL,
-     Id_prodotto_ordinato CHAR(50) NOT NULL,
-     Quantita INT NOT NULL,
-     Prezzo_unitario DECIMAL(10,2) NOT NULL,
-     Prezzo_totale DECIMAL(10,2) NOT NULL,
-     PRIMARY KEY (Id_prodotto_ordinato, Id_ordine),
-     FOREIGN KEY (Id_prodotto_ordinato) REFERENCES PRODOTTO_ORDINATO(Id_prodotto_ordinato),
-     FOREIGN KEY (Id_ordine) REFERENCES ORDINE(Id_ordine)
-);
+create table prodotto_ordinato (
+     Id_ordine char(1) not null,
+     Id_prodotto char(1) not null,
+     Quantita char(1) not null,
+     primary key (Id_ordine, Id_prodotto),
+     foreign key (Id_prodotto) references PRODOTTO (Id_prodotto),
+     foreign key (Id_ordine) references ORDINE (Id_ordine));
+     
+create table RECENSIONE (
+     Id__recensione char(1) not null,
+     data char(1) not null,
+     Voto char(1) not null,
+     Commento char(1) not null,
+     E_mail char(1) not null,
+     primary key (Id__recensione),
+     foreign key (E_mail) references UTENTE (E_mail));
 
-CREATE TABLE AGGIORNAMENTO (
-     Id_stato CHAR(50) NOT NULL,
-     Tipo_aggiornamento CHAR(255) NOT NULL,
-     Testo CHAR(255) NOT NULL,
-     Giorno DATE NOT NULL,
-     Ora TIME NOT NULL,
-     Id_ordine CHAR(50) NOT NULL,
-     PRIMARY KEY (Id_stato),
-     FOREIGN KEY (Id_ordine) REFERENCES ORDINE(Id_ordine)
-);
+-- Constraints Section
+-- ___________________ 
 
-CREATE TABLE RECENSIONE (
-     Id_recensione CHAR(50) NOT NULL,
-     Data DATE NOT NULL,
-     Voto INT NOT NULL CHECK (Voto BETWEEN 1 AND 5),
-     Commento CHAR(255) NOT NULL,
-     PRIMARY KEY (Id_recensione)
-);
-
-CREATE TABLE REALIZZAZIONE (
-     Id_recensione CHAR(50) NOT NULL,
-     E_mail CHAR(255) NOT NULL,
-     PRIMARY KEY (E_mail, Id_recensione),
-     FOREIGN KEY (Id_recensione) REFERENCES RECENSIONE(Id_recensione),
-     FOREIGN KEY (E_mail) REFERENCES UTENTE(E_mail)
-);
-
-CREATE TABLE INTERAGISCE (
-     Id_prodotto CHAR(50) NOT NULL,
-     E_mail CHAR(255) NOT NULL,
-     Tipo_interazione CHAR(50) NOT NULL,
-     PRIMARY KEY (Id_prodotto, E_mail),
-     FOREIGN KEY (Id_prodotto) REFERENCES PRODOTTO(Id_prodotto),
-     FOREIGN KEY (E_mail) REFERENCES UTENTE(E_mail)
-);
-
--- Indices Section
+/*alter table ORDINE add constraint ID_ORDINE_CHK
+     check(exists(select * from prodotto_ordinato
+                  where prodotto_ordinato.Id_ordine = Id_ordine));*/
+                  
+-- Index Section
 -- _____________ 
-CREATE UNIQUE INDEX IDX_UTENTE_EMAIL ON UTENTE (E_mail);
-CREATE UNIQUE INDEX IDX_METODO_PAGAMENTO_ID ON METODO_DI_PAGAMENTO (Id_pagamento);
-CREATE UNIQUE INDEX IDX_METODO_SPEDIZIONE_ID ON METODO_DI_SPEDIZIONE (Id_spedizione);
-CREATE UNIQUE INDEX IDX_INDIRIZZO_FATTURA_ID ON INDIRIZZO_DI_FATTURAZIONE (Id_fattura);
-CREATE UNIQUE INDEX IDX_ORDINE_ID ON ORDINE (Id_ordine);
-CREATE UNIQUE INDEX IDX_PRODOTTO_ID ON PRODOTTO (Id_prodotto);
-CREATE UNIQUE INDEX IDX_PRODOTTO_ORDINATO_ID ON PRODOTTO_ORDINATO (Id_prodotto_ordinato);
-CREATE UNIQUE INDEX IDX_RECENSIONE_ID ON RECENSIONE (Id_recensione); 
+
+create unique index ID_NOTIFICA_IND
+     on NOTIFICA (Id_notifica);
+
+create index FKcontrollo_IND
+     on NOTIFICA (E_mail);
+
+create unique index ID_CATEGORIA_IND
+     on CATEGORIA (Nome_categoria);
+
+create unique index ID_METODO_DI_PAGAMENTO_IND
+     on METODO_DI_PAGAMENTO (Id_pagamento);
+
+create unique index ID_METODO_DI_SPEDIZIONE_IND
+     on METODO_DI_SPEDIZIONE (Id_spedizione);
+
+create unique index ID_ORDINE_IND
+     on ORDINE (Id_ordine);
+
+create index FKspedizione_IND
+     on ORDINE (Id_spedizione);
+
+create index FKpagamento_IND
+     on ORDINE (Id_pagamento);
+
+create index FKeffettuazione_IND
+     on ORDINE (E_mail);
+
+create unique index ID_PRODOTTO_IND
+     on PRODOTTO (Id_prodotto);
+
+create index FKpresenta_IND
+     on PRODOTTO (Nome_categoria);
+
+create unique index ID_carrello_IND
+     on carrello (E_mail, Id_prodotto);
+
+create index FKcar_PRO_IND
+     on carrello (Id_prodotto);
+
+create unique index ID_preferito_IND
+     on preferito (Id_prodotto, E_mail);
+
+create index FKpre_UTE_IND
+     on preferito (E_mail);
+
+create unique index ID_prodotto_ordinato_IND
+     on prodotto_ordinato (Id_ordine, Id_prodotto);
+
+create index FKpro_PRO_IND
+     on prodotto_ordinato (Id_prodotto);
+
+create unique index ID_RECENSIONE_IND
+     on RECENSIONE (Id__recensione);
+
+create index FKrealizzazione_IND
+     on RECENSIONE (E_mail);
+
+create unique index ID_UTENTE_IND
+     on UTENTE (E_mail);
--- template/base.php
+++ template/base.php
@@ -11,40 +11,53 @@
 </head>
 <body>
     <header>
-        <button class="btn p-0" style="background-color: white" type="button" data-bs-toggle="offcanvas" data-bs-target="#staticBackdrop" aria-controls="staticBackdrop">
-            <i class="bi bi-list ps-3" style="color: rgb(137, 85, 32); font-size: 50px;"></i>
+        <button class="btn" style="background-color: white" type="button" data-bs-toggle="offcanvas" data-bs-target="#staticBackdrop" aria-controls="staticBackdrop">
+            <i class="bi bi-list" style="color: rgb(137, 85, 32); font-size: 50px;"></i>
         </button>
+
         <div class="offcanvas offcanvas-start offcanvas-custom" data-bs-backdrop="static" tabindex="-1" id="staticBackdrop" aria-labelledby="staticBackdropLabel">
-            <div class="offcanvas-header">
-                <p>Menù</p>
-                <button type="button" style="background-color: white;" class="btn" data-bs-dismiss="offcanvas" aria-label="Close">
-                    <i class="bi bi-x-lg" style="color:rgb(137, 85, 32); font-size: 35px"></i>
+            <div class="offcanvas-header pb-0">
+                <button type="button" style="background-color: white;" class="btn ms-auto" data-bs-dismiss="offcanvas" aria-label="Close">
+                    <i class="bi bi-x-lg ps-2" style="color:rgb(137, 85, 32); font-size: 35px"></i>
                 </button>
             </div>
-                <div class="offcanvas-body">
-                    <div>
-                        <form class="d-flex">
-                            <input class="form-control me-2" type="text" placeholder="Search">
-                            <button class="btn" style="color: rgb(137, 85, 32);" type="button">
-                                <i class="bi bi-search-heart" style="font-size: 30px;"></i>
-                            </button>
-                        </form>
-                        <a href="#">Account<br></a>
-                        <a href="#">Tutti i nostri peluche<br></a>
-                        <a href="#">Animali<br></a>
-                        <a href="#">Cibo<br></a>
-                    </div>
+            <div class="offcanvas-body">
+                <div>
+                    <form class="d-flex">
+                        <input class="form-control shadow-brown" type="text" placeholder="Search">
+                        <button class="btn" style="color: rgb(137, 85, 32);" type="button">
+                            <i class="bi bi-search-heart" style="font-size: 30px;"></i>
+                        </button>
+                    </form>
+                    <a href="#">Account<br></a>
+                    <a href="#">Lista Desideri<br></a>
+                    <a href="#">Tutti i nostri peluche<br></a>
+                    <a href="#">Animali<br></a>
+                    <a href="#">Fantasy<br></a>
+                    <a href="#">Piante<br></a>    
                 </div>
+            </div>
         </div>
-        <img src="img/img.png"/>
-        <i class="bi bi-bag pe-3" style="color: rgb(137, 85, 32); font-size: 40px;"></i>
+
+        <img src="img/logopeluche.png" alt="Logo"/>
+
+        <!--<button class="btn d-none d-md-block ms-30%" type="button">
+            <i class="bi bi-heart" style="color: rgb(137, 85, 32); font-size: 40px"></i>
+        </button>
+        <button class="btn d-none d-md-block ms-auto" type="button">
+            <i class="bi bi-person" style="color: rgb(137, 85, 32); font-size: 45px"></i>        
+        </button>-->
+
+        <button class="btn" type="button">
+            <i class="bi bi-bag" style="color: rgb(137, 85, 32); font-size: 40px;"></i>            
+        </button>
     </header>
     <footer>
         <p>Informazioni su Mondo Morbidoso<br></p>
         <p>Assistenza Clienti.<br></p>
         <p>Informativa Privacy.<br></p>
     </footer>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" 
-integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" 
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 </html>
