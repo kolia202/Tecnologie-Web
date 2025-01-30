6eb2a9379e3f6f1418dff6e337aaa680fbe840f3
Sara Panfini <sara.panfini@studio.unibo.it>
2025-01-30 22:35:58 +0100

    messo i peluche

--- db/Popolamento.sql
+++ db/Popolamento.sql
@@ -43,31 +43,46 @@ INSERT INTO METODO_DI_SPEDIZIONE (Nome, Descrizione, Costo) VALUES
 
 -- Popolamento della tabella PRODOTTO
 INSERT INTO PRODOTTO (Nome, Descrizione, Immagine, Grandezza, Scorta, Prezzo, Prezzo_punti, Nome_categoria) VALUES
-('Orsetto Teo', 'Un dolce orsetto morbido da abbracciare', '', 'M', 50, 19.99, 200, 'Orsetti'),
-('Orsetto Oliver', 'Tenero orsetto con cuore ricamato sulla pancia', '', 'S', 40, 14.99, 150, 'Orsetti'),
-('Orsetto Marcus', 'Orsetto in peluche con grandi occhi tondi', '', 'M', 30, 24.99, 250, 'Orsetti'),
-('Orsetto Joel', 'Orsetto polare bianco in peluche morbido', '', 'L', 20, 29.99, 300, 'Orsetti'),
-('Orsetto Teddy', 'Classico orsetto Teddy di peluche', '', 'M', 25, 22.99, 220, 'Orsetti'),
+('Orsetto Teo', "L'orsetto Teo è un peluche vivace e curioso, con morbido pelo color nocciola e occhietti brillanti pieni di dolcezza. Adora le coccole e le avventure, ciò lo rende il perfetto compagno di giochi.", 'OrsettoTeo.png', 'S', 50, 34.99, 350, 'Orsetti'),
+('Orsetto Oliver', "L'orsetto Oliver è un peluche elegante e dolce, con un soffice pelo marrone. Sempre gentile e premuroso, ama le coccole e ascoltare storie prima di andare a dormire. Con il suo sguardo tenero e il cuore grande, Oliver è il perfetto amico per momenti di comfort e affetto.", 'OrsettoOliver.png', 'S', 40, 34.99, 350, 'Orsetti'),
+('Orsetto Marcus', "L'orsetto Marcus è un peluche forte e protettivo, con un morbido pelo color cioccolato e un'espressione dolce ma sicura. Sempre pronto a vegliare sui suoi amici, ama le avventure ma anche i lunghi abbracci. Marcus è sicuramente il compagno perfetto per sentirsi al sicuro e coccolati.", 'OrsettoMarcus.png', 'M', 30, 59.99, 600, 'Orsetti'),
+('Orsetto Joel', "L'orsetto Joel è un tenero orsetto polare, con un morbido manto bianco come la neve e occhietti scintillanti pieni di dolcezza. Ama rannicchiarsi al caldo nelle fredde notti invernali e regalare abbracci soffici come fiocchi di neve. Con il suo musetto delicato e il cuore gentile, Joel è il compagno perfetto per coccole senza fine.", 'OrsettoJoel.png', 'M', 20, 59.99, 600, 'Orsetti'),
+('Orsetta Puppy', "L'orsetta Puppy è un peluche dolce e giocoso, con un soffice pelo rosa e un nasino che la rende irresistibile. Sempre allegra e affettuosa, adora le coccole e le avventure spensierate. Con il suo cuore tenero, Puppy è la compagna perfetta per momenti di gioia e tenerezza.", 'OrsettaPuppy.png', 'S', 25, 19.99, 200, 'Orsetti'),
+('Orsetto Teddy', "L'orsetto Teddy è un peluche dal cuore grande e dal pelo morbido color marrone scuro. Con il suo sguardo affettuoso, è il compagno ideale per chi cerca conforto e compagnia. Sempre pronto a regalare abbracci, Teddy è l'amico perfetto per ogni momento speciale.", 'OrsettoTeddy.png', 'S', 10, 22.99, 230, 'Orsetti'),
+('Orsetto Charlie', "L'orsetto Charlie è un peluche tenero e affettuoso, con un pelo morbido color cioccolato che lo rende irresistibile al tatto. Ha occhi brillanti e un sorriso rassicurante ed è sempre pronto a portare conforto e calore a chi lo abbraccia. Charlie è l'amico perfetto per ogni momento di coccole e gioia.", 'OrsettoCharlie.png', 'S', 15, 17.99, 180, 'Orsetti'),
+('Orsetto Fluffy', "L'orsetto Fluffy è un peluche soffice e coccoloso, con un pelo morbidissimo marrone chiaro e un musetto dolce che ispira tenerezza. Sempre pronto a farsi stringere in abbracci avvolgenti, ama le coccole e i sonnellini tranquilli. Con il suo aspetto adorabile e il cuore gentile, Fluffy è il compagno perfetto per momenti di dolcezza infinita.", 'OrsettoFluffy.png', 'S', 40, 29.99, 300, 'Orsetti'),
+('Orsetto Max', "L'orsetto Max è un peluche vivace e affettuoso, con il pelo morbido e un'espressione curiosa e simpatica. Sempre pronto all'avventura, ama giocare ma anche regalare abbracci rassicuranti. Con il suo sorriso dolce, Max è il perfetto compagno di giochi e coccole.", 'OrsettoMax.png', 'S', 30, 9.99, 100, 'Orsetti'),
+('Orsetto Ettore', "L'orsetto Ettore è un peluche dolce e protettivo, con un morbido pelo color crema e occhi profondi pieni di gentilezza. Calmo e rassicurante, ama stare accanto ai suoi amici e offrire abbracci soffici. Con il suo aspetto affettuoso e il cuore grande, Ettore è il compagno ideale per momenti di coccole e serenità.", 'OrsettoEttore.png', 'S', 5, 17.99, 180, 'Orsetti');
+
+INSERT INTO PRODOTTO (Nome, Descrizione, Immagine, Grandezza, Scorta, Prezzo, Prezzo_punti, Nome_categoria) VALUES
 ('Leone Narciso', 'Leone in peluche grande con criniera dorata', '', 'L', 15, 34.99, 350, 'Animali'),
 ('Elefante Kevin', 'Elefante morbido con grandi orecchie', '', 'XL', 10, 39.99, 400, 'Animali'),
 ('Zebra Oscar', 'Zebra con pelliccia a strisce bianche e nere', '', 'M', 30, 26.99, 270, 'Animali'),
 ('Giraffa Daisy', 'Giraffa a peluche alta e snodabile', '', 'M', 20, 28.99, 280, 'Animali'),
-('Tigre Bruce', 'Tigre in peluche con strisce arancioni e nere', '', 'L', 25, 32.99, 330, 'Animali'),
+('Tigre Bruce', 'Tigre in peluche con strisce arancioni e nere', '', 'L', 25, 32.99, 330, 'Animali');
+
+INSERT INTO PRODOTTO (Nome, Descrizione, Immagine, Grandezza, Scorta, Prezzo, Prezzo_punti, Nome_categoria) VALUES
 ('Topolino', 'Peluche del famoso Topolino Disney', '', 'S', 40, 19.99, 200, 'Disney'),
 ('Stitch', 'Il peluche perfetto per ogni fan di Lilo&Stitch', '', 'S', 35, 18.99, 190, 'Disney'),
 ('Olaf', 'Peluche di Olaf morbido e coccoloso', '', 'M', 50, 22.99, 220, 'Disney'),
 ('Dumbo', 'Peluche del tenero elefantino Dumbo', '', 'M', 30, 24.99, 250, 'Disney'),
-('Simba', 'Peluche del Re della savana Simba', '', 'M', 45, 25.99, 270, 'Disney'),
+('Simba', 'Peluche del Re della savana Simba', '', 'M', 45, 25.99, 270, 'Disney');
+
+INSERT INTO PRODOTTO (Nome, Descrizione, Immagine, Grandezza, Scorta, Prezzo, Prezzo_punti, Nome_categoria) VALUES
 ('SpongeBob', 'Peluche di SpongeBob SquarePants', '', 'M', 40, 17.99, 180, 'Cartoni animati'),
 ('Pikachu', 'Peluche del famoso Pikachu di Pokémon', '', 'S', 50, 21.99, 220, 'Cartoni animati'),
 ('Shrek', 'Peluche di Shrek, il famoso orco verde', '', 'L', 20, 29.99, 300, 'Cartoni animati'),
 ('Scooby-Doo', 'Peluche di Scooby-Doo, il famoso cane detective', '', 'M', 30, 23.99, 240, 'Cartoni animati'),
-('Tom', 'Peluche del gatto di Tom&Jerry', '', 'M', 35, 25.99, 260, 'Cartoni animati'),
+('Tom', 'Peluche del gatto di Tom&Jerry', '', 'M', 35, 25.99, 260, 'Cartoni animati');
+
+INSERT INTO PRODOTTO (Nome, Descrizione, Immagine, Grandezza, Scorta, Prezzo, Prezzo_punti, Nome_categoria) VALUES
 ('Hamburger Peluche', 'Peluche a forma di hamburger', '', 'S', 40, 12.99, 130, 'Cibo'),
 ('Pizza Peluche', 'Peluche a forma di pizza', '', 'M', 50, 14.99, 150, 'Cibo'),
 ('Avocado Peluche', 'Peluche a forma di avocado', '', 'S', 60, 9.99, 100, 'Cibo'),
 ('Gelato Peluche', 'Peluche a forma di cono gelato', '', 'M', 30, 16.99, 170, 'Cibo'),
-('Donut Peluche', 'Peluche a forma di ciambella', '', 'S', 45, 11.99, 120, 'Cibo'),
+('Donut Peluche', 'Peluche a forma di ciambella', '', 'S', 45, 11.99, 120, 'Cibo');
+
+INSERT INTO PRODOTTO (Nome, Descrizione, Immagine, Grandezza, Scorta, Prezzo, Prezzo_punti, Nome_categoria) VALUES
 ('Cactus Peluche', 'Cactus in peluche verde', '', 'S', 35, 19.99, 200, 'Piante'),
 ('Fiore Peluche', 'Fiore colorato in peluche', '', 'M', 40, 22.99, 230, 'Piante'),
 ('Pianta Grassa Peluche', 'Peluche a forma di pianta grassa', '', 'M', 30, 25.99, 250, 'Piante'),
