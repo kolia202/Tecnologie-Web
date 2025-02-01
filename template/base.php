43a1b641643d25be33cf9a717fe363d2f423e9d5
kolia202 <kolia.zanni@gmail.com>
2025-02-01 10:36:37 +0100

    ahhh non so come si faa

--- template/base.php
+++ template/base.php
@@ -74,9 +74,66 @@
         </button>-->
 
         <!-- BOTTONE CARRELLO -->
-        <a href="../php/carrello.php" class="btn" style="height: 70%;">
-            <i class="bi bi-bag" style="color: rgb(137, 85, 32); font-size: 40px;"></i>  
-        </a>
+         <!-- Pulsante con icona e badge -->
+          <button class="btn position-relative" style="height: 70%;" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">
+            <i class="bi bi-bag" style="color: rgb(137, 85, 32); font-size: 40px;"></i>
+            <span class="position-absolute top-50 start-75 translate-middle badge rounded-pill bg-danger" style="font-size: 10px; padding: 6px 6px;">
+                0
+            </span>
+        </button>
+        <div class="offcanvas offcanvas-end offcanvas-custom" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
+            <div class="offcanvas-header">
+                <h5 class="offcanvas-title" id="offcanvasRightLabel">Carello</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <div class="offcanvas-body">
+            <h1>Carrello</h1>
+
+<?php if(count($templateParams["prodotti"]) == 0): ?>
+    <article>
+        <p>Il tuo carrello è vuoto</p>
+        <div class="text-center">
+            <a href="../php/prodotti.php">
+                <button type="button" class="btn btn-sm fw-bold" style="background-color: rgb(137, 85, 32); color: white; font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif; font-size: 12px; font-style: italic;">Torna al negozio</button>
+            </a>
+        </div>
+    </article>
+<?php
+    else:
+        foreach($templateParams["prodotti"] as $prodotto): ?>
+            <section>
+                <a href="../php/dettaglioProdotto.php?id=<?php echo $prodotto["Id_prodotto"]; ?>">
+                    <img src="<?php echo IMG_DIR.$prodotto["Immagine"]; ?>" alt=""/>
+                    <h2>Prodotto: <?php echo $prodotto["Nome"]; ?></h2>
+                </a>
+                <a href="../php/gestisci-carrello.php?action=2&id=<?php echo $prodotto["Id_prodotto"]; ?>">
+                    <button type="button" class="btn">
+                        <i class="bi bi-trash3-fill"></i>
+                    </button>
+                </a>
+                <div class="d-flex">
+                    <p>Quantità : </p>
+                    <button type="button" class="btn decrease" id="<?php echo $prodotto["Id_prodotto"]; ?>">
+                        <i class="bi bi-dash-circle"></i>
+                    </button>                
+                    <p class="quantity"><?php echo $prodotto["Quantita"]; ?></p>
+                    <button type="button" class="btn increase" id="<?php echo $prodotto["Id_prodotto"]; ?>">
+                        <i class="bi bi-plus-circle"></i>
+                    </button>
+                </div>
+                <p class="product-price">Prezzo: <?php echo getFormattedPrice(($prodotto["Prezzo"] * $prodotto["Quantita"])); ?></p>
+                <p class="product-points">Punti: <?php echo $prodotto["Prezzo_punti"] * $prodotto["Quantita"]; ?></p>
+            </section>
+        <?php endforeach; ?>
+        <h2 class="total-price">Totale Carrello: <?php echo getFormattedPrice($totale); ?></h2>
+        <div class="text-center">
+            <a href="#">
+                <button type="button" class="btn btn-sm fw-bold" style="background-color: rgb(137, 85, 32); color: white; font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif; font-size: 12px; font-style: italic;">Procedi con l'ordine</button>
+            </a>
+        </div>
+<?php endif; ?>     
+            </div>
+        </div>
     </header>
 
     <!-- MAIN -->
