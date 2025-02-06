8b8e183cf299e08b74d07055aa481ba4c6e1fe6d
Sara Panfini <sara.panfini@studio.unibo.it>
2025-02-06 16:22:34 +0100

    uffa

--- template/lista-prodotti.php
+++ template/lista-prodotti.php
@@ -1,49 +1,67 @@
-<?php if ($category): ?>
-    <h1><?php echo $category; ?></h1>
-<?php elseif(isset($_GET["search"])): ?>
-    <h1>Risultati per '<?php echo $_GET["search"] ?>'</h1>
-<?php else: ?>
-    <h1 class="favorite-store-titlee">Tutti i peluches <i class="bi bi-stars"></i></h1>
-<?php endif; ?>
-<?php if(isAdminLoggedIn()): ?>
-    <button type="button" class="btn btn-primary ms-5" data-bs-toggle="modal" data-bs-target="#aggiungiprodotto">
-        Aggiungi Prodotto
-    </button>
-<?php endif; ?>
-<?php foreach($templateParams["prodotti"] as $prodotto): ?>
-    <a class="dettaglio" href="../php/dettaglioProdotto.php?id=<?php echo $prodotto["Id_prodotto"]; ?>">
-        <img class="fotoProdotto" src="<?php echo IMG_DIR.$prodotto["Immagine"]; ?>" alt="Foto prodotto"/>
-        <h2 class="nomeProdotto"><?php echo $prodotto["Nome"]; ?></h2>
-    </a>
-    <?php if(isAdminLoggedIn() && $prodotto['attivo'] == 0): ?>
-        <span type="button" class="btn btn-sm fw-bold" style="background-color: black; color: white; font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif; font-size: 12px;">Prodotto Disattivato</span>
-    <?php elseif($prodotto["Scorta"] <= 0): ?>
-        <span type="button" class="btn btn-sm fw-bold" style="background-color: black; color: white; font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif; font-size: 12px;">Esaurito</span>
-    <?php endif; ?>
-    <p class="pProdotto"><?php echo getFormattedPrice($prodotto["Prezzo"]); ?></p>
-    <?php if(isAdminLoggedIn()): ?>
-        <div class="d-flex justify-content-between ms-5 me-5">        
-            <a href="../php/dettaglioProdotto.php?id=<?php echo $prodotto["Id_prodotto"]; ?>" type="button" class="btn btn-sm fw-bold" style="background-color: rgb(137, 85, 32); color: white; font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif; font-size: 12px; font-style: italic;">
-                Gestisci prodotto
-            </a>
-            <form action="gestisci-prodotto.php" method="POST">
-                <input type="hidden" name="previous" value="<?php echo $_SERVER['REQUEST_URI'] ?>">
-                <button type='submit' class='btn btn-primary' name='eliminaprodotto' value="<?php echo $prodotto['Id_prodotto']; ?>">Elimina Prodotto</button>
-            </form>
-        </div>
+<div class="container mt-4 ps-4 pe-4">
+    <?php if ($category): ?>
+        <h1 class="text-center favorite-store-titlee"><?php echo $category; ?></h1>
+    <?php elseif(isset($_GET["search"])): ?>
+        <h1 class="text-center favorite-store-titlee">Risultati per '<?php echo $_GET["search"] ?>'</h1>
     <?php else: ?>
-        <div class="text-center">        
-            <button type="button" class="btn btn-sm fw-bold add-to-cart" id="<?php echo $prodotto["Id_prodotto"]; ?>">
-                Aggiungi al carrello
+        <h1 class="text-center favorite-store-titlee">
+            Tutti i peluches
+            <i class="bi bi-stars"></i>
+        </h1>
+    <?php endif; ?>
+
+    <?php if(isAdminLoggedIn()): ?>
+        <div class="text-center mt-5 mb-2">
+            <button type="button" class="btn btn-primary aggiungiprodottob ps-4 pe-4 fw-bold" data-bs-toggle="modal" data-bs-target="#aggiungiprodotto">
+                Aggiungi Prodotto
             </button>
         </div>
     <?php endif; ?>
-<?php endforeach; ?>
+
+    <div class="row mt-4">
+        <?php foreach($templateParams["prodotti"] as $prodotto): ?>
+            <div class="col-12 col-md-4 mb-5">
+                <a class="dettaglio" href="../php/dettaglioProdotto.php?id=<?php echo $prodotto["Id_prodotto"]; ?>">
+                    <img class="fotoProdotto img-fluid" src="<?php echo IMG_DIR.$prodotto["Immagine"]; ?>" alt="Foto prodotto"/>
+                    <h2 class="nomeProdotto text-center mt-3"><?php echo $prodotto["Nome"]; ?></h2>
+                </a>
+                <div class="text-center">
+                    <?php if(isAdminLoggedIn() && $prodotto['attivo'] == 0): ?>
+                        <span class="btn btn-sm fw-bold blackb mb-2">Prodotto Disattivato</span>
+                    <?php elseif($prodotto["Scorta"] <= 0): ?>
+                        <span class="btn btn-sm fw-bold blackb mb-2">Esaurito</span>
+                    <?php endif; ?>
+                </div>
+                <p class="text-center pProdotto"><?php echo getFormattedPrice($prodotto["Prezzo"]); ?></p>
+
+                <?php if(isAdminLoggedIn()): ?>
+                    <div class="d-flex justify-content-between ms-4 me-4">        
+                        <a href="../php/dettaglioProdotto.php?id=<?php echo $prodotto["Id_prodotto"]; ?>" type="button" class="btn btn-sm fw-bold gestiscip">
+                            Gestisci prodotto
+                        </a>
+                        <form action="gestisci-prodotto.php" method="POST">
+                            <input type="hidden" name="previous" value="<?php echo $_SERVER['REQUEST_URI'] ?>">
+                            <button type='submit' class='btn btn-danger btn-sm eliminap' name='eliminaprodotto' value="<?php echo $prodotto['Id_prodotto']; ?>">Elimina Prodotto</button>
+                        </form>
+                    </div>
+                <?php else: ?>
+                    <div class="text-center">        
+                        <button type="button" class="btn btn-sm fw-bold add-to-cart" id="<?php echo $prodotto["Id_prodotto"]; ?>">
+                            Aggiungi al carrello
+                        </button>
+                    </div>
+                <?php endif; ?>
+            </div>
+        <?php endforeach; ?>
+    </div>
+</div>
+
+
 <div class="modal fade" id="aggiungiprodotto" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered">
         <div class="modal-content">
             <div class="modal-header">
-                <h1 class="modal-title fs-5" id="staticBackdropLabel">Nuovo Prodotto</h1>
+                <h1 class="modal-titlep fs-5" id="staticBackdropLabel">Nuovo Prodotto</h1>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <form action="gestisci-prodotto.php" method="POST" enctype="multipart/form-data">
@@ -51,33 +69,33 @@
                     <label for="immagineprodotto"></label>
                     <input type="file" id="immagineprodotto" name="immagineprodotto" accept="image/*" required/>
                     <br><label for="nomeprodotto"></label>
-                    <input type="text" id="nomeprodotto" name="nomeprodotto" placeholder="Nome" required/>
+                    <input class="mt-3 modal-text" type="text" id="nomeprodotto" name="nomeprodotto" placeholder="Nome" required/>
                     <br><label for="categoriaprodotto"></label>
-                    <select id="categoriaprodotto" name="categoriaprodotto" required>
-                        <option value="" disabled selected>Categoria</option>
+                    <select class="mt-3" id="categoriaprodotto" name="categoriaprodotto" required>
+                        <option class="modal-text" value="" disabled selected>Categoria</option>
                         <?php foreach($templateParams["categorie"] as $categoria): ?>
-                            <option value="<?php echo $categoria["Nome_categoria"]; ?>"><?php echo $categoria["Nome_categoria"] ?></option>
+                            <option class="modal-text" value="<?php echo $categoria["Nome_categoria"]; ?>"><?php echo $categoria["Nome_categoria"] ?></option>
                         <?php endforeach; ?>
                     </select>
                     <br><label for="tagliaprodotto"></label>
-                    <select id="tagliaprodotto" name="tagliaprodotto" required>
-                        <option value="" disabled selected>Taglia</option>
-                        <option value="S">S - Small</option>
-                        <option value="M">M - Medium</option>
-                        <option value="L">L - Large</option>
+                    <select class="mt-3" id="tagliaprodotto" name="tagliaprodotto" required>
+                        <option class="modal-text" value="" disabled selected>Taglia</option>
+                        <option class="modal-text" value="S">S - Small</option>
+                        <option class="modal-text" value="M">M - Medium</option>
+                        <option class="modal-text" value="L">L - Large</option>
                     </select>
                     <br><label for="prezzoprodotto"></label>
-                    <input type="number" step="0.01" min="0" id="prezzoprodotto" name="prezzoprodotto" placeholder="Prezzo" required/>
+                    <input class="modal-text mt-3" type="number" step="0.01" min="0" id="prezzoprodotto" name="prezzoprodotto" placeholder="Prezzo" required/>
                     <br><label for="puntiprodotto"></label>
-                    <input type="number" id="puntiprodotto" name="puntiprodotto" min="0" placeholder="Punti" required/>
+                    <input class="mt-3 modal-text" type="number" id="puntiprodotto" name="puntiprodotto" min="0" placeholder="Punti" required/>
                     <br><label for="scorteprodotto"></label>
-                    <input type="number" id="scorteprodotto" name="scorteprodotto" min="1" placeholder="Scorte" required/>
-                    <br><label for="descrizioneprodotto">Descrizione:</label>
-                    <textarea rows="10" style="width: 100%" id="descrizioneprodotto" name="descrizioneprodotto" placeholder="Scrivi qui..." required/></textarea>
+                    <input class="mt-3 modal-text" type="number" id="scorteprodotto" name="scorteprodotto" min="1" placeholder="Scorte" required/>
+                    <br><label class="mt-3 modal-text" for="descrizioneprodotto">Descrizione:</label>
+                    <textarea class="modal-text" rows="10" style="width: 100%" id="descrizioneprodotto" name="descrizioneprodotto" placeholder="Scrivi qui..." required/></textarea>
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annulla</button>
-                    <button type="submit" class="btn btn-primary" name="aggiungiprodotto">Salva</button>
+                    <button type="button" class="btn modallink" data-bs-dismiss="modal">Annulla</button>
+                    <button type="submit" class="btn btn-primary modalbutton ps-4 pe-4" name="aggiungiprodotto">Salva</button>
                 </div>
             </form>
         </div>
