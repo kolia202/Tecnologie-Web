deddad0b6f1c7935261324f6b0136a45bde7f050
kolia202 <kolia.zanni@gmail.com>
2025-02-06 22:36:08 +0100

    a

--- template/pagina-pagamento.php
+++ template/pagina-pagamento.php
@@ -1,8 +1,7 @@
-<h1>Pagamento</h1>
-
+<h1 class="total-price">Pagamento <i class="bi bi-credit-card"></i></h1>
 <form method="POST" action="pagamento.php">
     <?php if (isset($_SESSION["paymenterror"])): ?>
-        <p style="color: red;"><?php echo $_SESSION["paymenterror"]; ?></p>
+        <p class="error"><?php echo $_SESSION["paymenterror"]; ?></p>
     <?php endif; ?>
     <section class="border-bottom">
         <?php foreach($templateParams["pagamenti"] as $pagamento): ?>
@@ -14,19 +13,19 @@
             </div>   
         <?php endforeach; ?>
     </section>
-    <p style="font-weight: bold;">Hai abbastanza punti per questo acquisto?</p>
-    <p>Sei a un passo dal completare l'acquisto! Se desideri utilizzare i tuoi punti fedeltà, puoi farlo qui.</p>
+    <p class="spedizione-punti">Hai abbastanza punti per questo acquisto?</p>
+    <p class="spiegazione-punti">Sei a un passo dal completare l'acquisto! Se desideri utilizzare i tuoi punti fedeltà, puoi farlo qui.</p>
     <p class="totale-punti">Per questo ordine sono necessari <?php echo $totalepunti; ?> punti</p>
-    <p class="punti-utente" style="display: none;"><?php echo $userDetails["Punti"] ?></p>
-    <button type="button" class="btn btn-outline paga-punti" style="border-color: rgb(137, 85, 32); color: rgb(137, 85, 32); font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif; font-size: 12px; font-style: italic;">Paga con i punti</button>
+    <p class="punti-utente"><?php echo $userDetails["Punti"] ?></p>
+    <button type="button" class="btn btn-outline paga-punti">Paga con i punti</button>
     <input type="hidden" name="usapunti" class="usa-punti" value="0">
-    <p class="errore-punti" style="display: none;"></p>
+    <p class="errore-punti"></p>
     <section>
         <h4 class="totale-carrello">Carrello: <?php echo getFormattedPrice($totale) ?></h4>
         <h4 class="costo-spedizione">Spedizione: <?php echo getFormattedPrice($costospedizione) ?></h4>
         <h2 class="totale-finale">Totale: <?php echo getFormattedPrice($totale + $costospedizione); ?></h2>   
     </section>
     <div class="text-center">
-        <button type="submit" name="invia" class="btn btn-sm fw-bold" style="background-color: rgb(137, 85, 32); color: white; font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif; font-size: 12px; font-style: italic;">Riepilogo Ordine</button>
+        <button type="submit" name="invia" class="btn btn-sm fw-bold custom-fw">Riepilogo Ordine</button>
     </div>
 </form>
