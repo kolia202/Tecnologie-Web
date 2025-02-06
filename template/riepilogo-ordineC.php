1c61ec740eae7ec2df980b04f4bcd6fbc8dcb293
Sara Panfini <sara.panfini@studio.unibo.it>
2025-02-06 23:37:49 +0100

    bg

--- template/riepilogo-ordineC.php
+++ template/riepilogo-ordineC.php
@@ -1,22 +1,22 @@
 <h1>Riepilogo Ordine</h1>
 
 <?php foreach($templateParams["carrello"] as $prodotto): ?>
-    <section class="d-flex align-items-center border-bottom">
-        <a href="../php/dettaglioProdotto.php?id=<?php echo $prodotto["Id_prodotto"]; ?>" style="width: 50%;">
-            <img src="<?php echo IMG_DIR.$prodotto["Immagine"]; ?>" class="img-fluid" alt=""/>
+    <div class="col-12 col-md-4 mb-5">
+    <section class="custom-col">
+        <a class="prodotto-carrello-a" href="../php/dettaglioProdotto.php?id=<?php echo $prodotto["Id_prodotto"]; ?>">
+            <img class="imgCar" src="<?php echo IMG_DIR.$prodotto["Immagine"]; ?>" class="img-fluid" alt=""/>
+            <h2 class="nomeProdottoC"><?php echo $prodotto["Nome"]; ?></h2>
         </a>
-        <div class="d-flex flex-column">
-            <a href="../php/dettaglioProdotto.php?id=<?php echo $prodotto["Id_prodotto"]; ?>">
-                <p><?php echo $prodotto["Nome"]; ?></p>
-            </a>
-            <p class="text-muted"><?php echo $prodotto["Quantita"] . "x" . getFormattedPrice($prodotto["Prezzo"]); ?></p>
-        </div>
+        <p class="text-muted"><?php echo $prodotto["Quantita"] . "x" . getFormattedPrice($prodotto["Prezzo"]); ?></p>
     </section>
+</div>
 <?php endforeach; ?>
-<h4>Spedizione: <?php echo getFormattedPrice($costospedizione) ?></h4>
-<h2>Totale: <?php echo getFormattedPrice($_SESSION["totalecarrello"] + $costospedizione); ?></h2>
+<h4 class="text-center">Spedizione: <?php echo getFormattedPrice($costospedizione) ?></h4>
+<h2 class="text-center">Totale: <?php echo getFormattedPrice($_SESSION["totalecarrello"] + $costospedizione); ?></h2>
 <form method="POST" action="aggiungi-ordine.php">
     <div class="text-center">
-        <button type="submit" class="btn btn-sm fw-bold" style="background-color: rgb(137, 85, 32); color: white; font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif; font-size: 12px; font-style: italic;">Conferma Ordine</button>
+        <button type="submit" class="btn btn-sm fw-bold mt-3 mb-4 ordinib" style="background-color: rgb(137, 85, 32); color: white; font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif; font-size: 12px; font-style: italic;">Conferma Ordine</button>
     </div>
 </form>
+
+
