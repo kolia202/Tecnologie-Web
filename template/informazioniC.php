f77dd7874e908962cee22e0c8a2d91a31d64cd86
kolia202 <kolia.zanni@gmail.com>
2025-02-07 00:37:04 +0100

    git

--- template/informazioniC.php
+++ template/informazioniC.php
@@ -1,50 +1,49 @@
-<h1>Informazioni su Mondo Morbidoso</h1>
-<h2>Chi siamo?</h2>
-<p>Benvenuti in <strong>Mondo Morbidoso</strong>,<br>
-posto dove la dolcezza prende vita! Siamo un’azienda nata dalla passione per i peluche<br>
-e il desiderio di regalare momenti di gioia e tenerezza a grandi e piccini.<br>
-Ogni peluche della nostra collezione è scelto con cura, realizzato con materiali di alta qualità<br>
+<h1 class="favorite-store-titlee">Informazioni <i class="bi bi-info-square"></i></h1>
+<h2 class="h2-info">Chi siamo?</h2>
+<p class="p-info">Benvenuti in <strong>Mondo Morbidoso</strong>,
+posto dove la dolcezza prende vita! Siamo un’azienda nata dalla passione per i peluche
+e il desiderio di regalare momenti di gioia e tenerezza a grandi e piccini.
+Ogni peluche della nostra collezione è scelto con cura, realizzato con materiali di alta qualità
 e pensato per essere il compagno perfetto per ogni occasione.</p> 
-<p>Il nostro obiettivo è creare un mondo di morbidezza e affetto,<br>
-dove ognuno possa trovare il peluche ideale, che sia un regalo speciale,<br>
-un ricordo d’infanzia o semplicemente un amico da abbracciare nei momenti di relax.<br>
-Grazie alla nostra attenzione ai dettagli e alla continua ricerca di novità,<br>
+<p class="p-info">Il nostro obiettivo è creare un mondo di morbidezza e affetto,
+dove ognuno possa trovare il peluche ideale, che sia un regalo speciale,
+un ricordo d’infanzia o semplicemente un amico da abbracciare nei momenti di relax.
+Grazie alla nostra attenzione ai dettagli e alla continua ricerca di novità,
 offriamo una gamma di peluche che spaziano dai classici orsetti alle creature più fantastiche.</p> 
-<p>In <strong>Mondo Morbidoso</strong>, crediamo che ogni peluche racconti una storia e porti con sé emozioni uniche.<br>
+<p class="p-info">In <strong>Mondo Morbidoso</strong>, crediamo che ogni peluche racconti una storia e porti con sé emozioni uniche.
 Per questo ci impegniamo a offrire solo il meglio, garantendo morbidezza, sicurezza e tanto amore in ogni prodotto.</p>
-<h2>Dove Trovarci?</h2>
-<p>Vuoi portare a casa un peluche soffice e adorabile? Con <strong>Mondo Morbidoso</strong>, è facile e veloce!</p> 
-<h3>Acquista Online</h3> 
-<p>Scopri la nostra collezione e ordina comodamente da casa! Visita il nostro shop online e scegli il tuo peluche <br>
+<h1 class="h2-info">Dove Trovarci?</h1>
+<p class="p-info">Vuoi portare a casa un peluche soffice e adorabile? Con <strong>Mondo Morbidoso</strong>, è facile e veloce!</p> 
+<h1 class="h2-info">Acquista Online</h1> 
+<p class="p-info">Scopri la nostra collezione e ordina comodamente da casa! Visita il nostro shop online e scegli il tuo peluche <br>
 <a href="http://localhost/tecnologie-web/php/" target="_blank">https://MondoMorbidoso.it</a></p>
-<h3>Punto Vendita</h3>
-<p>Ti aspettiamo nel nostro negozio per toccare con mano tutta la morbidezza di <strong>Mondo Morbidoso</strong>!<br>
+<h1 class="h2-info">Punto Vendita</h1>
+<p class="p-info">Ti aspettiamo nel nostro negozio per toccare con mano tutta la morbidezza di <strong>Mondo Morbidoso</strong>!
 Indirizzo: Via Cesare Pavese, 50, 47521 Cesena FC<br>
-Orari di apertura: dal lunedi al venerdì dalle 7:00 - 14:00 e dalle 15:00 alle 19:00<br></p>
-<div style="width: 100%; max-width: 400px; margin: 0 auto; text-align: center;">
+Orari di apertura: dal lunedi al venerdì<br> dalle 7:00 - 14:00 e dalle 15:00 alle 19:00<br></p>
+<div class="mappa">
     <iframe 
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2862.880073567643!2d12.231737257602665!3d44.147717335371816!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x132ca4c8a9e0b5cb%3A0xbbd5902e81162eed!2sDipartimento%20di%20Informatica%20-%20Scienza%20e%20Ingegneria%20-%20Universit%C3%A0%20di%20Bologna!5e0!3m2!1sit!2sit!4v1738576296068!5m2!1sit!2sit" 
         width="90%" height="250" 
-        style="border: 2px solid rgb(137, 85, 32); display: block; margin: 0 auto;" 
         allowfullscreen="" 
         loading="lazy">
     </iframe>
 </div>
-<h2>Contattaci</h2>
-<p>Telefono: 0547338888</p>
-<p>Email: 
+<h1 class="h2-info">Contattaci</h1>
+<p class="p-info">Telefono: 0547338888</p>
+<p class="p-info">Email: 
     <a href="mailto:MondoMorbidoso@gmail.com">
         <i class="bi bi-envelope"></i> MondoMorbidoso@gmail.com
     </a>
 </p>
-<p>Social:
+<p class="p-info">Social:
     <a href="https://instagram.com" target="_blank">
-        <i class="bi bi-instagram" style="color:#E1306C;"></i> Instagram
+        <i class="bi bi-instagram"></i> Instagram
     </a> |  
     <a href="https://facebook.com" target="_blank">
-        <i class="bi bi-facebook" style="color:#1877F2;"></i> Facebook
+        <i class="bi bi-facebook"></i> Facebook
     </a> |  
     <a href="https://tiktok.com" target="_blank">
-        <i class="bi bi-tiktok" style="color:#000;"></i> TikTok
+        <i class="bi bi-tiktok"></i> TikTok
     </a>
 </p>
