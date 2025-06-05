38626a9a4d22c0a3b64506078b6b0b2bff08fbcc
Sara Panfini <sara.panfini@studio.unibo.it>
2025-06-05 21:02:44 +0200

    uffa

--- template/conferma-ordine.php
+++ template/conferma-ordine.php
@@ -1,10 +1,12 @@
-<h1 class="mt-4">Grazie per il tuo ordine!</h1>
-<p class="mt-3 ordineconferma">Il tuo ordine è stato ricevuto con successo!</p>
-<p class="ordineconferma">Puoi controllare lo stato dell'ordine all'interno del tuo profilo.</p>
-<p class="ordineconferma">Grazie per aver scelto Mondo Morbidoso!</p>
-<p class="ordineconferma">Speriamo che il tuo nuovo amico di peluche ti porti tanta felicità!</p>
-<div class="text-center mt-5">        
-    <a href="../php/index.php" class="btn fw-bold confermab">
-            Torna alla Home
-    </a>
+<div class="container mt-4 ps-4 pe-4">
+    <h1 class="title">Grazie per il tuo ordine!</h1>
+    <p class="text mt-3">
+        Il tuo ordine è stato completato con successo!
+        Puoi controllare lo stato dell'ordine all'interno del tuo profilo.<br/>
+        Grazie per aver scelto Mondo Morbidoso!
+        Speriamo che il tuo nuovo amico di peluche ti porti tanta felicità!
+    </p>
+    <div class="text-center mt-4">        
+        <a href="../php/index.php" class="btn button">Torna alla Home</a>
+    </div>
 </div>
